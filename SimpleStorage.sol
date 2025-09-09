a39fefe7f88e9c3c9d4648e93f6b3a9415563b43
Oluwa-Laughter <ismakinde@gmail.com>
2025-09-09 12:37:04 +0100

    functions, array and struct

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,15 +2,41 @@
 pragma solidity ^0.8.18; 
 
 contract SimpleStorage {
-     // Basic types
-    bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 88;
-    string favoriteNumberInText = "eighty-eight";
-    int256 favoriteInt = -88;
-    address myAddress = 0x99c9347D21E571AA497f4bE993CB26B08aF48744;
-    bytes32 favoriteBytes32 = "cat";
+    // Basic types
+    // bool hasFavoriteNumber = true;
+    // uint256 favoriteNumber = 88;
+    // string favoriteNumberInText = "eighty-eight";
+    // int256 favoriteInt = -88;
+    // address myAddress = 0x99c9347D21E571AA497f4bE993CB26B08aF48744;
+    // bytes32 favoriteBytes32 = "cat";
+
+    uint256 myFavoriteNumber;
+
+    uint256[] listOfFavouriteNumer;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public hey = Person({favoriteNumber: 8, name:'Tim'});
+
+
+    
+    Person[] public listOfPeople;
 
     function store(uint256 _favouriteNumber) public  {
-        favoriteNumber = _favouriteNumber;
+        myFavoriteNumber = _favouriteNumber;
+    }
+
+    function retrive() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        // Person memory newPerson = Person(_favouriteNumber, _name);
+        // listOfPeople.push(newPerson);
+
+        listOfPeople.push( Person(_favouriteNumber, _name));
     }
 }
